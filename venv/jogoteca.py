9802e4aa9407fd9b2df13952259cf5ce82cb3836
masson90 <andremasson.10@gmail.com>
2024-09-03 08:57:26 -0300

    feat:conecao+banco

--- venv/jogoteca.py
+++ venv/jogoteca.py
@@ -1,6 +1,5 @@
-# from flask import Flask, render_template 
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask_sqlalchemy import SQLAlchemy
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
@@ -12,7 +11,33 @@ jogo2 = Jogo('God of War', 'Rack n Slash', 'Playstation')
 jogo3 = Jogo('Mortal Kombat', 'Luta', 'Super Nintendo')
 lista = [jogo1, jogo2, jogo3]
 
+class Usuario:
+    def __init__(self, nome, nickname, senha):
+        self.nome = nome
+        self.nickname = nickname
+        self.senha = senha  
+
+usuario1 = Usuario('Andre Luis', 'andre', '123')
+usuario2 = Usuario('Lucas Lorran', 'lucas', '123')
+usuario3 = Usuario('Cleiber Costa', 'cleiber', '123')
+
+usuarios = {usuario1.nickname : usuario1,
+            usuario2.nickname : usuario2,
+            usuario3.nickname : usuario3 }
+
 app = Flask(__name__)
+app.secret_key = 'alura'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    '{SGBD}://{usuario}:{senha}@{servidor}:{database}'.format(
+        SGBD='mysql+mysqlconnector',
+        usuario='root',
+        senha='123456',
+        servidor='localhost',
+        database='jogoteca' 
+    )
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index(): 
@@ -20,6 +45,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))  
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
@@ -29,7 +56,32 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)    
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' Login efetuado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+    else:
+        flash('Usuário ou senha inválidos!')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))    
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
